59071931bf48bc2c875afe9ff9ac060fe272d585
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-21 13:42:15 +0530

    Create BinarySearch.java

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class BinarySearch {
+    public static int binary(int arr[], int key, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key == arr[mid]) {
+                return mid;
+            } else if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 4, 6, 8, 10, 12, 14 };
+        int key = 4;
+        int r = binary(arr, key, 0, arr.length - 1);
+        if (r != -1) {
+            System.out.println("Key found at index :" + r);
+        } else {
+            System.out.println("Key not found");
+        }
+    }
+}
