4333325897cb2412fdcae7223eb0b2ecbe83301f
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-22 17:00:38 +0530

    Create reverse.java

---
+++ Arrays/reverse.java
@@ -0,0 +1,17 @@
+public class reverse {
+    public static void reverseA(int arr[]) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        reverseA(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
