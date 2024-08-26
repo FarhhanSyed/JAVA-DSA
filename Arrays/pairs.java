2f034449feeabbf02fb9ade11fb0abb8e431fadd
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-27 01:47:34 +0530

    Update pairs.java

--- Arrays/pairs.java
+++ Arrays/pairs.java
@@ -2,8 +2,9 @@ public class pairs {
     public static void pairing(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                System.out.println("(" + arr[i] + "," + arr[j] + ")");
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
             }
+            System.out.println();
         }
     }
 
