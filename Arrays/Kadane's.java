9155f4f5ecb58c8af1d2f1d2af6c81aa3bb4a223
Syed Farhan <163603911+FarhhanSyed@users.noreply.github.com>
2024-09-28 22:28:19 +0530

    Update Kadane's.java

--- Arrays/Kadane's.java
+++ Arrays/Kadane's.java
@@ -1,1 +1,23 @@
+public class kadanes {
+    public static void subarrSum(int arr[]) {
+        int sum = 0;
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+            if (sum > max) {
+                max = sum;
+            }
+
+        }
+        System.out.println("Max: " + max);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        subarrSum(arr);
+    }
+}
 
