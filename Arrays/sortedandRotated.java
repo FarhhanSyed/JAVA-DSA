eda99dc6094949c9d5a1a483d20814ffce5d4618
Syed Farhan <163603911+FarhhanSyed@users.noreply.github.com>
2024-10-01 23:10:54 +0530

    Add files via upload

---
+++ Arrays/sortedandRotated.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class sortedandRotated {
+    public static boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % 5]) {
+                count++;
+            }
+        }
+        if (count > 1)
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 4, 5, 1, 2 };
+        System.out.println(check(arr));
+    }
+}
