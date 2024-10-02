ed6b63ba4e97b6b84a1484b2b6eeb277f5eedb0a
Syed Farhan <163603911+FarhhanSyed@users.noreply.github.com>
2024-10-02 14:28:36 +0530

    Create bubble_sort.java

---
+++ Soritng/bubble_sort.java
@@ -0,0 +1,23 @@
+//Timr Complexity:O(n^2)
+import java.util.*;
+
+public class bubble_sort {
+    public static void bubble(int arr[]) {
+        int n = arr.length;
+        for (int turn = 0; turn <= n - 2; turn++) {
+            for (int j = 0; j <= n - 2 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 6, 2, 7, 3, 1 };
+        bubble(arr);
+    }
+}
