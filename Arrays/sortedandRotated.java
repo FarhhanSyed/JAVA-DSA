0bb5fc02ddc47bdb8414bb375fcbd2d7edc57443
Syed Farhan <163603911+FarhhanSyed@users.noreply.github.com>
2024-10-02 13:45:11 +0530

    Update sortedandRotated.java

--- Arrays/sortedandRotated.java
+++ Arrays/sortedandRotated.java
@@ -5,13 +5,13 @@ public class sortedandRotated {
     public static boolean check(int[] nums) {
         int n = nums.length;
         int count = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % 5]) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
                 count++;
+                if (count > 1)
+                    return false;
             }
         }
-        if (count > 1)
-            return false;
         return true;
     }
 
