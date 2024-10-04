d36b4c477444f72eef95313730966d21330c5e0c
Syed Farhan <163603911+FarhhanSyed@users.noreply.github.com>
2024-10-04 16:34:13 +0530

    Create right_rotate.java

---
+++ Arrays/right_rotate.java
@@ -0,0 +1,28 @@
+//Time Complexity:O(2n)
+import java.util.*;
+
+public class right_rotate {
+    public static void reverse(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void right(int arr[], int k) {
+        int n = arr.length;
+        reverse(arr, 0, n - k - 1);
+        reverse(arr, n - k, n - 1);
+        reverse(arr, 0, n - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        right(arr, k);
+    }
+}
