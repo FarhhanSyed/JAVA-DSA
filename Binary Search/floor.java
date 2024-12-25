41576cc8d4a186f6bc0eda8abdbcc2694498a47b
FarhhanSyed <farhansyed2045@gmail.com>
2024-12-25 08:59:27 +0530

    floor number in an array

---
+++ Binary Search/floor.java
@@ -0,0 +1,23 @@
+public class floor {
+    public static int floorNumber(int arr[], int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target < arr[mid]) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return arr[e];
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 2, 3, 4, 5, 9, 14, 16, 18 };
+        int target = 15;
+        System.out.println(floorNumber(arr, target));
+    }
+}
