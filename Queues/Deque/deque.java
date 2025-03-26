f4c03962ec1d93e293c936caea0a84d84a41e096
FarhhanSyed <farhansyed2045@gmail.com>
2025-03-26 15:04:50 +0530

    deque

---
+++ Queues/Deque/deque.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class deque{
+    public static void main(String args[])
+    {
+        Deque<Integer> dq=new LinkedList<>();
+        dq.addFirst(4);
+        dq.addFirst(8);
+        dq.addFirst(3);
+        System.out.println(dq);
+        dq.addLast(1);
+        dq.addLast(6);
+        System.out.println(dq);
+        System.out.println(dq.removeFirst());
+        System.out.println(dq.removeLast());
+        System.out.println(dq);
+    }
+}
