4c6b011bddaa5d8561487851dfdc7ada3b1932b8
FarhhanSyed <farhansyed2045@gmail.com>
2025-03-29 20:42:21 +0530

    representation of trees using linked list

---
+++ Trees/representation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class representation {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static int idx = -1;
+
+    public static Node buildTree(int nodes[]) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 8, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = buildTree(nodes);
+        System.out.println(root.data);
+    }
+}
