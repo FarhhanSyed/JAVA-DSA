dd5be8429a1338b43b66e754ab22acc1e8a343ae
Syed Farhan <farhansyed2045@gmail.com>
2025-06-04 20:47:58 +0530

    Aggressive coes

--- Binary Search/aggressiveCows.java
+++ Binary Search/aggressiveCows.java
@@ -1,7 +1,10 @@
+
 // You are given an array with unique elements of stalls[], which denote the position of a stall.
 // You are also given an integer k which denotes the number of aggressive cows. 
 // Your task is to assign stalls to k cows such that the minimum distance between any two of them
 // is the maximum possible.
+import java.util.*;
+
 public class aggressiveCows {
     public static boolean canWePlace(int[] arr, int cur, int k) {
         int last = arr[0];
@@ -16,6 +19,7 @@ public class aggressiveCows {
     }
 
     public static int maxDistance(int[] arr, int k) {
+        Arrays.sort(arr);
         int mini = Integer.MAX_VALUE;
         int maxi = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
